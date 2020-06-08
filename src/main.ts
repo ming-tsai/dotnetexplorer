df5ef4e071b281b2a6051a8fcfda98ebcda1bd94
Ming Tsai <ming.jia1213@gmail.com>
2020-06-07 21:38:08 -0400

    fix: some time not found buefy components

--- src/main.ts
+++ src/main.ts
@@ -12,17 +12,18 @@ import "buefy/dist/buefy.css";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
-
 library.add(fas);
 library.add(fab);
 Vue.component("vue-fontawesome", FontAwesomeIcon);
 
 Vue.use(Buefy, {
   defaultIconComponent: "vue-fontawesome",
-  defaultIconPack: "fas"
+  defaultIconPack: "fas",
+  defaultContainerElement: "app"
 });
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
